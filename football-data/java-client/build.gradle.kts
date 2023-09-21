6bef824be58758d3ba275e24765d59efd4702c42
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-09-21 23:35:05 +0200

    chore(release): fix gradle build script ?

--- football-data/java-client/build.gradle.kts
+++ football-data/java-client/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 configure<ReleaseExtension>  {
     val module = project.path.split(":")[1]
 
-    tagTemplate.set("$module-$version")
+    tagTemplate.set("$module-\${version}")
     preTagCommitMessage.set("release($module) - pre tag commit: ")
     tagCommitMessage.set("release($module) - creating tag: ")
     newVersionCommitMessage.set("release($module) - new version commit: ")
