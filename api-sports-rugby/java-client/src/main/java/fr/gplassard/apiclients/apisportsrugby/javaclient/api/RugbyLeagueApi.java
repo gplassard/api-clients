a55e485c32cb62cfaf98de7a29762cd2f91cb853
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-12-27 23:36:45 +0100

    feat(high-level-api): create a high level client (#8)
    * feat(high-level-api): create a high level client
    
    * feat(high-level-api): fix error handling
    
    * feat(high-level-api): enable integration tests
    
    * feat(high-level-api): fix tests
    
    * feat(high-level-api): add test for bad api key

---
+++ api-sports-rugby/java-client/src/main/java/fr/gplassard/apiclients/apisportsrugby/javaclient/api/RugbyLeagueApi.java
@@ -0,0 +1,46 @@
+package fr.gplassard.apiclients.apisportsrugby.javaclient.api;
+
+import fr.gplassard.apiclients.apisportsrugby.javaclient.ApiClient;
+import fr.gplassard.apiclients.apisportsrugby.javaclient.ApiException;
+import fr.gplassard.apiclients.apisportsrugby.javaclient.api.base.LeagueApi;
+import fr.gplassard.apiclients.apisportsrugby.javaclient.model.League;
+import fr.gplassard.apiclients.apisportsrugby.javaclient.model.LeagueType;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class RugbyLeagueApi {
+    private final LeagueApi leagueApi;
+
+    public RugbyLeagueApi(String apiKey) {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setRequestInterceptor(r -> r.headers("x-apisports-key", apiKey));
+        this.leagueApi = new LeagueApi();
+    }
+
+    public List<League> listLeagues(String id, String name, String countryId, LeagueType type, String season, String search) throws ApiException {
+         var response = this.leagueApi.listLeaguesWithHttpInfo(id, name, countryId, type, season, search);
+         var errors = this.getErrors(response.getData().getErrors());
+         if (!errors.isEmpty()) {
+             throw new ApiException(
+                     response.getStatusCode(),
+                     errors.toString()
+             );
+         }
+         return response.getData().getResponse();
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Map<String, Object>> getErrors(Object errors) {
+        // errors can either be a JSON array of objects or directly an object if there is only one error
+        if (errors == null) {
+            return Collections.emptyList();
+        }
+        if (errors instanceof List) {
+            return (List<Map<String, Object>>) errors;
+        }
+        return Collections.singletonList((Map<String, Object>) errors);
+    }
+
+}
