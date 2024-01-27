f57a3c9e49df81793249d08f93639fdb929cf13d
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-01-27 01:22:17 +0100

    chore(java-21): bump to java 21 (#4)
    * chore(java-21): bump to java 21
    
    * chore(java-21): bump to gradle 8.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    // https://docs.gradle.org/8.0.2/userguide/toolchains.html#sub:download_repositories
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
+
 rootProject.name = "api-clients"
 
 include("football-data:smithy")
